3027c645e6a58f243c2a5eae71df9d07bf92c3f4
Ahnseongmin1749 <csi1368@gmail.com>
2025-08-04 18:51:02 +0900

    Update MiddleSave.cs

--- Assets/Scenes/MiddleSave.cs
+++ Assets/Scenes/MiddleSave.cs
@@ -6,7 +6,7 @@ public class MiddleSave : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log(midlesave);   
     }
 
     // Update is called once per frame
